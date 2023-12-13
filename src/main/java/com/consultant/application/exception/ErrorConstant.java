720afb0c8b3262ceb03760038bbd4cec8ebb91f6
KimHyunJin <khj745700@naver.com>
2023-12-14 00:20:58 +0900

    GlobalExceptionHandler 추가
    - @RestControllerAdvice를 활용하여 예외 발생시 에러 메세지가 반환되도록 설정
    - CustomException 을 활용하여 예외를 반환할 것
    - ErrorConstant 에 예외를 상수로 지정해 GC 타겟에 빠지도록 할 것

---
+++ src/main/java/com/consultant/application/exception/ErrorConstant.java
@@ -0,0 +1,24 @@
+package com.consultant.application.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ErrorConstant {
+    INTERNAL_SERVER_ERROR("알 수 없는 오류가 발생했습니다." , HttpStatus.INTERNAL_SERVER_ERROR),
+    STUDENT_NOT_FOUND("학생을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+
+    STUDENT_IS_RETIRED("학생을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    STAFF_NOT_FOUND("직원을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    STAFF_IS_RETIRED("직원이 퇴직하였습니다", HttpStatus.NOT_FOUND),
+    CONSULTING_NOT_FOUND("상담 내역을 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
+
+
+    private final String message;
+    private final HttpStatus httpStatus;
+
+    ErrorConstant(String message, HttpStatus httpStatus) {
+        this.message = message;
+        this.httpStatus = httpStatus;
+    }
+}
