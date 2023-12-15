ed3529645407a221770fac8d5a3b56bbaafa1ede
KimHyunJin <khj745700@naver.com>
2023-12-15 11:13:31 +0900

    Dto Validation 예외 처리

--- src/main/java/com/consultant/application/exception/GlobalExceptionHandler.java
+++ src/main/java/com/consultant/application/exception/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.consultant.application.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.Arrays;
@@ -24,14 +26,28 @@ public class GlobalExceptionHandler {
         return ExceptionResponse.toResponseEntity(customException.getErrorConstant());
     }
 
-    static class ExceptionResponse extends ResponseEntity<Object> {
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception){
+        String exceptionMessage = exception.getAllErrors().get(0).getDefaultMessage();
+        return ExceptionResponse.toResponseEntity(exceptionMessage, HttpStatus.BAD_REQUEST);
+    }
 
+    static class ExceptionResponse extends ResponseEntity<Object> {
+        private static final String BODY_FORMAT = "{\n  \"message\": \"%s\"\n}";
         private ExceptionResponse(ErrorConstant errorConstant) {
-            super("message : " + errorConstant.getMessage(), errorConstant.getHttpStatus());
+            super(String.format(BODY_FORMAT, errorConstant.getMessage()), errorConstant.getHttpStatus());
+        }
+
+        private ExceptionResponse(String message, HttpStatus status) {
+            super(String.format(BODY_FORMAT, message), status);
         }
 
         private static ResponseEntity<Object> toResponseEntity(ErrorConstant errorConstant) {
             return  new ExceptionResponse(errorConstant);
+        }
+
+        private static ResponseEntity<Object> toResponseEntity(String message, HttpStatus status) {
+            return new ExceptionResponse(message, status);
         }
     }
 }
