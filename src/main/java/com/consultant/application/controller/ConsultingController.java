9a269ff713f244b5817d8e880c3671cd9152ec4a
KimHyunJin <khj745700@naver.com>
2023-12-15 11:19:01 +0900

    Swagger 문서 작성

--- src/main/java/com/consultant/application/controller/ConsultingController.java
+++ src/main/java/com/consultant/application/controller/ConsultingController.java
@@ -8,10 +8,16 @@ import com.consultant.application.entity.consulting.Consulting;
 import com.consultant.application.service.consulting.FindConsultingService;
 import com.consultant.application.service.consulting.ModifyConsultingService;
 import com.consultant.application.service.consulting.RegisterConsultingService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -22,12 +28,22 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/consulting")
+@Tag(name = "상담", description = "상담 API")
 public class ConsultingController {
     private final RegisterConsultingService registerConsultingService;
     private final ModifyConsultingService modifyConsultingService;
     private final FindConsultingService findConsultingService;
 
     @PostMapping("")
+    @Operation(summary = "상담 등록 API", description = " 학생과 상담원 간의 상담 정보를 등록하는 API")
+    @ApiResponses(value =  {
+            @ApiResponse(responseCode = "201", description = "생성됨", content = @Content(schema = @Schema(implementation = ConsultingResponse.class))),
+            @ApiResponse(responseCode = "400", description = "파라미터가 정상적으로 입력되지 않은 경우", content = @Content(schema = @Schema(example = "{\n\t\"message\" : \"값이 유효하지 않습니다. (???는 필수 입력입니다.)\"\n}"))),
+            @ApiResponse(responseCode = "404", description = "학생이 존재하지 않는 경우", content = @Content(schema = @Schema(example = "{\n\t\"message\" : \"학생을 찾을 수 없습니다.\"\n}"))),
+            @ApiResponse(responseCode = "404 ", description = "학생이 퇴원한 경우", content = @Content(schema = @Schema(example = "{\n\t\"message\" : \"퇴원한 학생입니다.\"\n}"))),
+            @ApiResponse(responseCode = "404  ", description = "직원이 존재하지 않는 경우", content = @Content(schema = @Schema(example = "{\n\t\"message\" : \"직원을 찾을 수 없습니다.\"\n}"))),
+            @ApiResponse(responseCode = "404   ", description = "직원이 퇴직한 경우", content = @Content(schema = @Schema(example = "{\n\t\"message\" : \"퇴직한 직원입니다.\"\n}")))
+    })
     public ResponseEntity<Object> saveConsulting(@RequestBody @Valid ConsultingRegisterRequest request) {
         Consulting consulting = registerConsultingService.registerConsulting(request);
         ConsultingResponse response = ConsultingResponse.of(consulting);
@@ -35,6 +51,15 @@ public class ConsultingController {
     }
 
     @PutMapping("")
+    @Operation(summary = "상담 피드백 등록/수정 API", description = "담당자가 피드백을 등록하는 API")
+    @ApiResponses(value =  {
+            @ApiResponse(responseCode = "200", description = "성공", content = @Content(schema = @Schema(implementation = ConsultingResponse.class))),
+            @ApiResponse(responseCode = "400", description = "파라미터가 정상적으로 입력되지 않은 경우", content = @Content(schema = @Schema(example = "{\n\t\"message\" : \"???를 입력해주세요.\"\n}"))),
+            @ApiResponse(responseCode = "404", description = "학생이 존재하지 않는 경우", content = @Content(schema = @Schema(example = "{\n\t\"message\" : \"학생을 찾을 수 없습니다.\"\n}"))),
+            @ApiResponse(responseCode = "404 ", description = "학생이 퇴원한 경우", content = @Content(schema = @Schema(example = "{\n\t\"message\" : \"퇴원한 학생입니다.\"\n}"))),
+            @ApiResponse(responseCode = "404  ", description = "직원이 존재하지 않는 경우", content = @Content(schema = @Schema(example = "{\n\t\"message\" : \"직원을 찾을 수 없습니다.\"\n}"))),
+            @ApiResponse(responseCode = "404   ", description = "직원이 퇴직한 경우", content = @Content(schema = @Schema(example = "{\n\t\"message\" : \"퇴직한 직원입니다.\"\n}")))
+    })
     public ResponseEntity<Object> consultingModify(@RequestBody @Valid ConsultingModifyRequest request) {
         Consulting consulting = modifyConsultingService.modifyConsulting(request);
         ConsultingResponse consultingResponse = ConsultingResponse.of(consulting);
@@ -43,9 +68,19 @@ public class ConsultingController {
 
 
     @GetMapping("/{consultingId}/{managerId}")
-    public ResponseEntity<Object> consultDetails(@PathVariable("consultingId")Long consultingId, @PathVariable("managerId") String managerId ) {
-        ConsultingModifyResponse consultingModifyResponse = ConsultingModifyResponse.of(findConsultingService.findConsulting(consultingId, managerId));
-        return ResponseEntity.ok(consultingModifyResponse);
+    @Operation(summary = "상담 상세 보기 및 담당자 읽음 처리 API", description = "상담 정보 조회 및 담당자 읽음 처리 API")
+    @ApiResponses(value =  {
+            @ApiResponse(responseCode = "200", description = "성공", content = @Content(schema = @Schema(implementation = ConsultingResponse.class))),
+            @ApiResponse(responseCode = "400", description = "파라미터가 정상적으로 입력되지 않은 경우", content = @Content(schema = @Schema(example = "{\n\t\"message\" : \"값이 유효하지 않습니다. (???는 필수 입력입니다.)\"\n}"))),
+            @ApiResponse(responseCode = "404", description = "학생이 존재하지 않는 경우", content = @Content(schema = @Schema(example = "{\n\t\"message\" : \"학생을 찾을 수 없습니다.\"\n}"))),
+            @ApiResponse(responseCode = "404 ", description = "학생이 퇴원한 경우", content = @Content(schema = @Schema(example = "{\n\t\"message\" : \"퇴원한 학생입니다.\"\n}"))),
+            @ApiResponse(responseCode = "404  ", description = "직원이 존재하지 않는 경우", content = @Content(schema = @Schema(example = "{\n\t\"message\" : \"직원을 찾을 수 없습니다.\"\n}"))),
+            @ApiResponse(responseCode = "404   ", description = "직원이 퇴직한 경우", content = @Content(schema = @Schema(example = "{\n\t\"message\" : \"퇴직한 직원입니다.\"\n}")))
+    })
+    public ResponseEntity<Object> consultDetails(@PathVariable("consultingId") @Schema(description = "상담 ID", example = "1") Long consultingId,
+                                                 @PathVariable("managerId")  @Schema(description = "담당자 ID", example = "abcd1234") String managerId ) {
+        ConsultingResponse consultingResponse = ConsultingResponse.of(findConsultingService.findConsulting(consultingId, managerId));
+        return ResponseEntity.ok(consultingResponse);
     }
 
     @GetMapping("")
